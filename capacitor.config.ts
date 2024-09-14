26633d6be333f963e452db4b1f2d99a8a525a35b
Moryab <ba.caamano@duocuc.cl>
2024-09-14 16:10:34 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Appduoc',
+  webDir: 'www'
+};
+
+export default config;

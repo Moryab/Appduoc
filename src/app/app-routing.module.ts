eb9384c4c16b9de2040b08fe8e4ef21d970c9ba0
moryab <ba.caamano@duocuc.cl>
2024-09-26 17:52:52 -0300

    router en agregar cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,32 +19,26 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
-
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
-
   {
     path: 'cursos',
     loadChildren: () => import('./pages/cursos/cursos.module').then( m => m.CursosPageModule)
   },
-
   {
     path: 'asistencias',
     loadChildren: () => import('./pages/asistencias/asistencias.module').then( m => m.AsistenciasPageModule)
   },
-
   {
     path: 'detalle-asistencia',
     loadChildren: () => import('./pages/detalle-asistencia/detalle-asistencia.module').then( m => m.DetalleAsistenciaPageModule)
   },
-
   {
     path: 'add-alum',
     loadChildren: () => import('./pages/add-alum/add-alum.module').then( m => m.AddAlumPageModule)
   },
-
   {
     path: 'agregar-curso',
     loadChildren: () => import('./pages/agregar-curso/agregar-curso.module').then( m => m.AgregarCursoPageModule)
